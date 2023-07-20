54638b3ca4693ef38da7709abc2f32558e392229
Izaque Rolim <98198585+IzaqueRolim@users.noreply.github.com>
2023-07-19 21:05:00 -0400

    Usuario e Pasta sendo criadas e relacionadas

---
+++ backend/src/main/java/com/uea/TesteWareFlow/dto/UsuarioPastaDTO.java
@@ -0,0 +1,13 @@
+package com.uea.TesteWareFlow.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class UsuarioPastaDTO {
+    private Long idUsuario;
+
+    private Long idPasta;
+}
--- backend/src/main/java/com/uea/TesteWareFlow/model/Pasta.java
+++ backend/src/main/java/com/uea/TesteWareFlow/model/Pasta.java
@@ -1,21 +1,25 @@
 package com.uea.TesteWareFlow.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "tb_pasta")
 public class Pasta {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String nome;
     private LocalDate data_criacao;
     private String rota_compartilhamento;
 
+    @ManyToMany
+    @JsonIgnoreProperties("{pastas}")
+    private List<Usuario> usuarios;
 }
